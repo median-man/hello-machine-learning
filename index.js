6cfc81aeca3bb85c7d1f259beb5f42f24966d751
John Desrosiers <johndrunner@yahoo.com>
2018-04-15 07:37:41 -0700

    Create index.js with simple linear regression feature.
    Create regression model by performing simple linear regression on
    data from advertising.csv.
    
    Add i/o for calculating predictions from the regression model.
    
    Part 1 of Hacker Noon article complete.

---
+++ index.js
@@ -0,0 +1,48 @@
+const ml = require('ml-regression');
+const csv = require('csvtojson');
+const { SLR } = ml; // Simple Linear Regression
+const readline = require('readline');
+
+const csvFilePath = 'advertising.csv';
+
+let csvData = [];
+let x = [];
+let y = [];
+
+let regressionModel;
+
+csv()
+  .fromFile(csvFilePath)
+  .on('json', (jsonObj) => {
+    csvData.push(jsonObj);
+  })
+  .on('done', () => {
+    dressData();
+    performRegression();
+  });
+
+function dressData() {
+  csvData.forEach((row) => {
+    x.push(parseFloat(row.radio));
+    y.push(parseFloat(row.sales));
+  });
+}
+
+function performRegression() {
+  regressionModel = new SLR(x, y);
+  const precision = 3;
+  console.log(regressionModel.toString(precision));
+  predictOutput();
+}
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+function predictOutput() {
+  rl.question('Enter input x for prediction (Press CTRL+C to exit): ', (answer) => {
+    console.log(`At x = ${answer}, y = ${regressionModel.predict(parseFloat(answer))}`);
+    predictOutput();
+  });
+}
